0202ddea87dcbca8612b32024ca2e057c09ad4b3
Shane McKee <Shane@dhcp-146-86-207-65.devices.wcslc.net>
2014-01-15 18:36:48 -0700

    Can now import into eclipse

---
+++ src/main/java/SimplestServer.java
@@ -0,0 +1,10 @@
+//import org.eclipse.jetty.server.Server;
+public class SimplestServer
+{
+    public static void main(String[] args) throws Exception
+    {
+//        Server server = new Server(8080);
+//        server.start();
+//        server.join();
+    }
+}
