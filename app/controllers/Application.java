0bdb4191b45c5ab7349c03530b4430babf87ce74
Shane McKee <Shane@dhcp-146-86-207-248.devices.wcslc.net>
2014-01-29 19:50:09 -0700

    mocked db and beginning of navigation after login.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,13 @@
 package controllers;
-import database.DatabaseConnectorDude;
+import java.io.File;
+
+import org.springframework.util.Assert;
+
 import play.mvc.Controller;
 import play.mvc.Http.RequestBody;
 import play.mvc.Result;
+import database.DatabaseConnectorDude;
+import database.DatabaseConnectorMocked;
 
 public class Application extends Controller {
 	DatabaseConnectorDude mDbConnection = new DatabaseConnectorDude();
@@ -16,12 +21,20 @@ public class Application extends Controller {
     	  RequestBody body = request().body();
     	  String username = body.asFormUrlEncoded().get("username")[0];
     	  String password = body.asFormUrlEncoded().get("password")[0];
-    	  //An experiment with my own little database that I have
-//    	  String query = "select * from People;";
-//    	  String result = DatabaseConnectorDude.query(query);
-    	  session("username", username);
-    	  session("password", password);
-    	  return ok(username + "\n" + password);
+    	  
+    	  //Not how it will actually look. I just need a database result of sorts. 
+    	  String dbPassword = DatabaseConnectorMocked.query("goodLogin");
+    	  
+    	  Assert.notNull(password, "How did the password end up being null? We're confused");
+    	  
+    	  if(password.equals(dbPassword)){
+    		  session("username", username);
+//        	  return ok(new File("public/html/welcome.html")); 
+    		  return ok();
+    	  } else {
+    		  return ok("Your username and password don't match anything in our records. Hint: username = \"admin\" and password = \"admin\" right now.");  
+    	  }
+    	  
     	}
 
 }
