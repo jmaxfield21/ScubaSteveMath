d6d9fe6831feb615b036efcec0bb85f9777a96a6
DillonIrish <Dillon.Irish@gmail.com>
2014-02-14 15:58:03 -0700

    Input pages for equation and range creation

---
+++ public/javascripts/Level1Creator.js
@@ -0,0 +1,5 @@
+function recognitionCreator(form) {
+    var number = form.inputbox.value;
+    alert(number);
+    //number to be set to DB as recognition value
+}
---
+++ public/javascripts/Level4_5Creator.js
@@ -0,0 +1,31 @@
+function equaitonCreator(form)
+{
+    alert("This shits working");
+    var firstNumber = form.firstNumber.value;
+    var secondNumber = form.secondNumber.value;
+    var operator = form.operator.value;
+    alert("Equation created: " +firstNumber+ ""+operator+ "" +secondNumber);
+    return;
+    //alert(firstNumber);
+    //alert(secondNumber);
+    //firstNumber and secondNumber to be sent to database
+}
+
+function rangeSetter(form)
+{
+    var firstRange = form.firstRange.value;
+    var secondRange = form.secondRange.value;
+    if(firstRange > secondRange)
+    {
+        alert("Range set: "+ secondRange+ "-" +firstRange);
+    }
+    else
+    {
+        alert("Range set: "+ firstRange+ "-" +secondRange);
+    }
+    return;
+    //alert(firstRange);
+    //alert(secondRange);
+    // firstRange and secondRange to be sent to database
+}
+
