e6132a7ce3c5ed48b2f364575f6de86842aa235e
Shane McKee <Shane@administrators-MacBook-Pro.local>
2014-02-19 11:59:58 -0700

    changes to start the add new user process

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -45,7 +45,47 @@ public class Application extends Controller {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return ok("We're so sorry... Something bad happened.");
+    }
+    
+    //untested
+    public static Result addUser(){
+    	if(!isLoggedIn()){
+    		return unauthorized("Scuba Steve wants you to login before proceeding!");
+    	}
+    	
+    	RequestBody body = request().body();
+    	String username = session("username");
+    	String password = body.asFormUrlEncoded().get("password")[0];
+    	
+    	String newUsername = body.asFormUrlEncoded().get("newUsername")[0];
+    	String newPassword = body.asFormUrlEncoded().get("newPassword")[0];
+    	
+    	String dbPassword = null;
+  	  	boolean isAdmin = false;
+  	  	
+  	  	//shouldn't ever be null since we already checked if they are logged in. 
+  	  	//If a null pointer happens here, I'm sorry about that bad logic
+  	  	isAdmin = new Boolean(session("isAdmin")).booleanValue();
+  	  
+		try {
+			ResultSet set = DatabaseConnectorDude.query("select password from login where username = \'" + username + "\';");
+			dbPassword = DatabaseConnectorDude.getStringsFromResultSet(set).get(0);
+			
+			if(!isAdmin){
+				return unauthorized("You must be an administrator to do this!");
+			}
+	    	  
+	    	if(password.equals(dbPassword)){
+	    		DatabaseConnectorDude.query(String.format("insert into login values ('%s','%s')", newUsername, newPassword));
+	    		return redirect("/adduser");
+	    	} else {
+	    		return unauthorized("Your password was incorrect");  
+	    	}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ok("We're so sorry... Something bad happened.");
     }
     
     public static Result logout() {
@@ -53,4 +93,11 @@ public class Application extends Controller {
     	return redirect("/login");
     }
 
+    private static boolean isLoggedIn(){
+    	String username = session("username");
+    	if(username != null){
+    		return true;
+    	}
+    	return false;
+    }
 }
