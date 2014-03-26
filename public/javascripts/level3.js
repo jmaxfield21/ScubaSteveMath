1378bc54340db1adbec2a45e70522236a8e52b89
DillonIrish <Dillon.Irish@gmail.com>
2014-03-26 14:28:03 -0600

    level3 basic game function

---
+++ public/javascripts/level3.js
@@ -0,0 +1,143 @@
+var index = 0;
+var score = 0;
+var correctAnswer;
+var leftButtonValue;
+var middleButtonValue;
+var rightButtonValue;
+
+//Called from html play button on click
+//Sets up the equation
+function setup()
+{
+	var wrong1 = randomAnswer(3);
+	var wrong2 = randomAnswer(3);
+	var answerArray = level3();
+	correctAnswer = answerArray[0] - answerArray[1];
+	setButtons(correctAnswer, wrong1, wrong2);
+	var playButton = document.getElementById("play");
+	playButton.style.display = "none";
+}
+
+//Called from setup()
+//Used to set up the multiple choice buttons
+function setButtons(correctAnswer, wrong1, wrong2)
+{
+	var randomAssignment = Math.floor(Math.random() * ( 3 - 1 + 1) ) + 1;
+	var randomAssignment2 = Math.floor(Math.random() * ( 2 - 1 + 1) ) + 1;
+	switch (randomAssignment)
+	{
+		case 1:
+			document.getElementById("answer1").innerHTML= correctAnswer;
+			leftButtonValue = correctAnswer;
+			if(randomAssignment2 == '1')
+				{
+					document.getElementById("answer2").innerHTML= wrong1;
+					middleButtonValue = wrong1;
+					document.getElementById("answer3").innerHTML= wrong2;
+					rightButtonValue = wrong2;
+				}
+			else
+				{
+					document.getElementById("answer2").innerHTML= wrong2;
+					middleButtonValue = wrong2;
+					document.getElementById("answer3").innerHTML= wrong1;
+					rightButtonValue = wrong1;
+				}
+			break;
+		case 2:
+			document.getElementById("answer2").innerHTML= correctAnswer;
+			middleButtonValue = correctAnswer;
+			if(randomAssignment2 == '1')
+				{
+					document.getElementById("answer1").innerHTML= wrong1;
+					leftButtonValue = wrong1;
+					document.getElementById("answer3").innerHTML= wrong2;
+					rightButtonValue = wrong2;
+				}
+			else
+				{
+					document.getElementById("answer1").innerHTML= wrong2;
+					leftButtonValue = wrong2;
+					document.getElementById("answer3").innerHTML= wrong1;
+					rightButtonValue = wrong1;
+				}
+			break;
+		case 3:
+			document.getElementById("answer3").innerHTML= correctAnswer;
+			rightButtonValue = correctAnswer;
+			if(randomAssignment2 == '1')
+				{
+					document.getElementById("answer2").innerHTML= wrong1;
+					middleButtonValue = wrong1;
+					document.getElementById("answer1").innerHTML= wrong2;
+					leftButtonValue = wrong2;
+				}
+			else
+				{
+					document.getElementById("answer2").innerHTML= wrong2;
+					middleButtonValue = wrong2;
+					document.getElementById("answer1").innerHTML= wrong1;
+					leftButtonValue = wrong1;
+				}
+			break;
+	}
+}
+
+//Called from isCorrect()
+//Accepts the percent of the tank that should be filled
+function changeHeightDynamic(percent)
+{
+
+	var input = percent * 100;
+	var currentEmpty = 100-input;
+	
+	//Converts  from number to string
+	currentEmpty = currentEmpty + "%";
+	
+	if(input == 0)
+	{
+		document.getElementById("emptyTank").style.height='100%';
+		document.getElementById("emptyTank").style.borderRadius = '0px';
+	}
+	else
+	{
+		document.getElementById("emptyTank").style.height=currentEmpty;
+		document.getElementById("emptyTank").style.borderBottomLeftRadius = '0px';
+		document.getElementById("emptyTank").style.borderBottomRightRadius = '0px';
+	}
+	return input;
+}
+
+//Called from html Answer buttons onclick
+//Accepts the students answer
+//Prints dialog along with adjusting tank
+function isCorrect(selectedButton)
+{
+	var studentAnswer;
+	switch(selectedButton)
+	{
+		case 'left':
+			studentAnswer = leftButtonValue;
+			break;
+		case 'middle':
+			studentAnswer = middleButtonValue;
+			break;
+		case 'right':
+			studentAnswer = rightButtonValue;
+			break;
+	}
+	if(studentAnswer == correctAnswer)
+	{
+		score++;
+		document.getElementById("result").innerHTML = 'Correct!';
+		changeHeightDynamic(score/index);
+	}
+	else
+	{
+		index++;
+		document.getElementById("result").innerHTML = 'Incorrect, the correct answer was ' +correctAnswer+ '.';
+		changeHeightDynamic(score/index);
+	}
+	
+	setup();
+}
